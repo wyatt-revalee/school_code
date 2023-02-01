4c08a69519f5d6c9f7d242c2898471553e9345dc
Wyatt Revalee <wyatt.revalee@gmail.com>
2023-02-01 09:06:50 -0500

    finished a1

--- S2023/458/assignments/a1/a1.c
+++ S2023/458/assignments/a1/a1.c
@@ -1,27 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int k;
+
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void permute(int index, int *arr, int n)
+{
+    if (index == n - 1)
+    {
+        int fixCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if(arr[i] == i+1)
+                fixCount++;
+        }
+        if(fixCount == k)
+        {
+            printf("[");
+            for (int i = 0; i < n; ++i)
+            {
+                if(i == n-1)
+                    printf("%d", arr[i]);
+                else
+                    printf("%d,", arr[i]);
+            }
+            printf("]\n");
+        }
+        return;
+    }
+    for (int i = index; i < n; i++)
+    {
+        swap(arr + index, arr + i);
+        permute(index + 1, arr, n);
+        swap(arr + i, arr + index);
+    }
+    return;
+}
 
 int main()
 {
-
     int n;
-    int k;
-
     printf("n:");
-    scanf("%d", &n);
+    if (scanf("%d", &n) != 1)
+        exit(1);
     printf("k:");
-    scanf("%d", &k);
-
+    if (scanf("%d", &k) != 1)
+        exit(1);
     if(k > n){
         printf("k must be <= n");
-        return 0;
+        exit(1);
     }
-    
-    int numbers[n];
-    for (int i = 1; i <= n; i++)
-        numbers[i-1] = i;
-    
-    for(int i = 0; i < sizeof(numbers)/4; i++)
-        printf("i:%d %d\n", i, numbers[i]);
-
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+        arr[i] = i + 1;
+    permute(0, arr, n);
     return 0;
 }
